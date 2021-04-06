b0e049e920091c0844fcd2de2d0a7e0215fb0394
Gonzalo Antonio Garcia Fernandez <garciafgonzalo@gmail.com>
2021-04-06 11:24:29 -0300

    docs agregado

---
+++ docs/precache-manifest.d26167ba2f432b4a4fa51f657c73b615.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b561c2649d5133748bb6324e0d77bc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db51a3009663bbd7f20a",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db51a3009663bbd7f20a",
+    "url": "/static/js/main.fd887c0e.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
